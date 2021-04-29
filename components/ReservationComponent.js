664d77966d61301227a90957a0563ea1f547d13a
Joseph Clark <jccode777@gmail.com>
2021-04-29 18:21:18 -0500

    Sending Emails

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -3,7 +3,7 @@ import { Text, View, ScrollView, StyleSheet,
     Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
-
+import * as Notifications from 'expo-notifications'
 
 
 class Reservation extends Component {
@@ -47,7 +47,9 @@ class Reservation extends Component {
                 },
                 {
                     text: 'OK',
-                    onPress: () => this.resetForm 
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                        this.resetForm()}
                         
                 }
             ],
@@ -63,6 +65,31 @@ class Reservation extends Component {
             date: new Date(),
             showCalendar: false,
         });
+    }
+
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+        let permissions = await Notifications.getPermissionsAsync();
+        if(!permissions.granted) {
+            permission = await Notifications.requestPermissionsAsync();
+        }
+        if(permissions.granted) {
+            sendNotification();
+        }
     }
 
     render() {
